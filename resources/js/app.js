97d792d85ae5ed50c6865265ad4059a8b3279991
dharmalesmana16 <dharmalesmana16@gmail.com>
2024-05-20 17:17:19 +0800

    first

---
+++ resources/js/app.js
@@ -0,0 +1,6 @@
+import './bootstrap';
+import Alpine from 'alpinejs';
+import 'bootstrap';
+window.Alpine = Alpine;
+
+Alpine.start();
---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,5 @@
+    <footer class="">
+
+        <marquee scrollamount="10" class="bg-danger py-1 display-1 fw-bold" >Terimakasih telah melakukan survei</marquee>
+
+    </footer>
