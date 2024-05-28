326ecf211cdb2da62b6f79f348feb440bb000ddb
dharma lesmana <dharmalesmana@dharmas-MacBook-Pro.local>
2024-05-28 23:11:25 +0800

    updateall

--- resources/views/backend/video/index.blade.php
+++ resources/views/backend/video/index.blade.php
@@ -7,7 +7,7 @@
             <div class="d-flex justify-content-between">
                 <div class="bd-highlight">
 
-                    <a href='/dashboard/video/new' class="btn btn-md btn-success">
+                    <a href='/dashboard/video/new' class="btn btn-md btn-success disabled">
                         Add New Data
                     </a>
 
@@ -26,28 +26,27 @@
     </div>
 
 
-    <div class="row row-cols-1 row-cols-md-3 g-4">
-        {{-- @foreach ($data as $res) --}}
-        <div class="col">
-            <div class="card myCard rounded">
-                {{-- <div class="text-center "> --}}
 
-                <video autoplay muted loop id="myVideo" width="350" class="card-img-top">
-                    <source src="/storage/{{ $data->file_video }}" type="video/mp4">
-                </video>
-                {{-- </div> --}}
-                <div class="card-body">
-                    <p class="fs-5 lead">Nama Video : <span> {{ $data['judul_video'] }}</span> </p>
-                    <p class="fs-5 lead">Tipe Video : .mp4 </p>
-                    {{-- <p class="fs-5 lead">Diupload : <span> {{ $res['created_at'] }}</span> </p> --}}
+    <div class="card myCard rounded">
+        {{-- <div class="text-center "> --}}
 
-                    {{-- <h3 class=" text-center"></h3> --}}
+        <video autoplay muted loop id="myVideo" width="350" class="card-img-top">
+            <source src="/storage/{{ $data->file_video }}" type="video/mp4">
+        </video>
+        {{-- </div> --}}
+        <div class="card-body">
+            <h5 class="card-title">Nama Video : <span> {{ $data['judul_video'] }}</span> </h5>
+            <div class="card-text">
+                <p class="fs-6 lead">Tipe Video : .mp4 </p>
 
-                </div>
+            </div>
+            {{-- <p class="fs-5 lead">Diupload : <span> {{ $res['created_at'] }}</span> </p> --}}
+
+            {{-- <h3 class=" text-center"></h3> --}}
+            <div class="mt-3">
+
                 <a href='/dashboard/video/update/{{ $data['id'] }}' class="btn btn-primary btn-lg">Edit</a>
             </div>
         </div>
-        {{-- @endforeach --}}
-
     </div>
 @endsection
